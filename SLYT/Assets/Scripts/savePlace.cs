e2d535d3de1bca77d9ad4368c670057cb98381d7
Qizi <32046016+531387937@users.noreply.github.com>
2018-07-09 16:29:12 +0800

    111

---
+++ SLYT/Assets/Scripts/savePlace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class savePlace : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerPrefs.SetFloat("save_x", transform.position.x);
+            PlayerPrefs.SetFloat("save_y", transform.position.y);
+
+        }
+    }
+}
