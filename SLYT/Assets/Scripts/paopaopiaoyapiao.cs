5abbf3b5338ad5253d227a0ab7c21c1375f84650
Zoryx <15801610913@163.com>
2018-07-10 13:44:21 +0800

    no message

--- SLYT/Assets/Scripts/paopaopiaoyapiao.cs
+++ SLYT/Assets/Scripts/paopaopiaoyapiao.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class paopaopiaoyapiao : MonoBehaviour {
     private PlayerCtr PlayerCtr;
-
+    public float max;
+    public float min;
     public GameObject player;
+    float t;
+    float timm;
+    float speed;
+        
+       
+    
     // Use this for initialization
     private void Awake()
     {
-        PlayerCtr = GetComponent<PlayerCtr>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        PlayerCtr =player. GetComponent<PlayerCtr>();
     }
     void Start () {
+        this.transform.localScale = new Vector3(1, 1, 1);
+        speed = Random.Range(0.6f, 1.2f);
 
-       
-	}
-	
+    }
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        this.transform.position += Vector3.up *speed  * Time.deltaTime;
+        timm += Time.deltaTime;
+        if(timm<0.3f)
+        {
+            t = timm * (10 / 3);
+            this.transform.localScale = new Vector3(Mathf.Lerp(min, max, t), Mathf.Lerp(min, max, t), Mathf.Lerp(min, max, t));
+
+        }
+       
+        if(this.transform.position.y>40)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
