ed90fd2254adaf5558b57f5fe85ff3a7d73031fd
Qizi <32046016+531387937@users.noreply.github.com>
2018-07-12 09:56:05 +0800

    443434

---
+++ SLYT/Assets/Scripts/collection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collection : MonoBehaviour {
+    public static int collect = 0;
+    // Use this for initialization
+    private void Awake()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            collect++;
+            print(collect);
+            Destroy(this.gameObject);
+        }
+    }
+}
