3d59b0694a16912d0410fdfa2055153092174e1b
PGOS\Administrator <531387937@qq.com>
2018-09-03 14:41:48 +0800

    传送带

---
+++ SLYT/Assets/Scripts/BeltMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltMove : MonoBehaviour {
+    public float moveSpeed;
+    public float rot;
+    public int direction;
+	// Use this for initialization
+	void Start () {
+        rot = this.gameObject.transform.localEulerAngles.z;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (direction >= 0) direction = 1;
+        if (direction < 0) direction = -1;
+      
+	}
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.position += new Vector3(moveSpeed * Mathf.Cos(rot / 180 * Mathf.PI), moveSpeed * Mathf.Sin(rot / 180 * Mathf.PI), 0)*Time.deltaTime*direction;
+        }
+    }
+    
+}
