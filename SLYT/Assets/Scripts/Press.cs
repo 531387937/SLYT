0466ddb66eb72707d197377d0fa21f61855c55fa
Qizi <32046016+531387937@users.noreply.github.com>
2018-09-04 18:42:04 +0800

    天花板陷阱

---
+++ SLYT/Assets/Scripts/Press.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Press : MonoBehaviour {
+    public GameObject trap;
+    public float trapSpeed;
+    public float backSpeed;
+    [SerializeField]
+    private bool canPress;
+    public float DownTime;
+    [SerializeField]
+    private float timer = 0;
+    private bool press = false;
+	// Use this for initialization
+	void Start () {
+        canPress = true;
+	}
+    private void Update()
+    {
+        if(press)
+        {
+            timer += Time.deltaTime;
+            if (timer < DownTime)
+            {
+                trap.transform.Translate(Vector3.down * trapSpeed * Time.deltaTime);
+            }
+            else
+            {
+                canPress = false;
+                press = false;
+                timer = 0;
+            }
+        }
+        if(!canPress)
+        {
+            timer += Time.deltaTime;
+            if (timer < DownTime * trapSpeed / backSpeed)
+            {
+                trap.transform.Translate(Vector3.up * backSpeed * Time.deltaTime);
+            }
+            else
+            {
+                timer = 0;
+                canPress = true;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player"&&canPress)
+        {
+            press = true;           
+        }
+    }
+}
