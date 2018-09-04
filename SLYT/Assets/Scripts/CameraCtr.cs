5b8ec21aec562f0c3b880f556e85ec55e2be7cba
Zoryx <15801610913@163.com>
2018-09-04 18:37:09 +0800

    第一部分 80%

--- SLYT/Assets/Scripts/CameraCtr.cs
+++ SLYT/Assets/Scripts/CameraCtr.cs
@@ -58,7 +58,7 @@ public class CameraCtr : MonoBehaviour {
         averagePos += Player2.transform.position;
 
 
-        averagePos = new Vector3(averagePos.x, averagePos.y, this.transform.position.z);
+        averagePos = new Vector3(averagePos.x, averagePos.y,-10);
 
         m_DesiredPosition = averagePos;
     }
