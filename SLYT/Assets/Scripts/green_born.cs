1161b89ad5bd5cab48a04877a75350ecd4e0def4
Qizi <531387937@qq.com>
2018-09-06 14:36:09 +0800

    绿色轨道球更改

--- SLYT/Assets/Scripts/green_born.cs
+++ SLYT/Assets/Scripts/green_born.cs
@@ -3,26 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class green_born : MonoBehaviour {
-    public GameObject green;
     GameObject son;
     float count = 0;
     public float time_;
-	// Use this for initialization
-	void Start () {
-        son=Instantiate(green, this.transform.position, Quaternion.Euler(Vector3.zero));
-        son.transform.SetParent(this.transform);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        son = gameObject.transform.GetChild(0).gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(son==null)
+		if(!son.activeInHierarchy)
         {
             count += Time.deltaTime;
             if(count>time_)
             {
                 count = 0;
-                son = Instantiate(green, this.transform.position, Quaternion.Euler(Vector3.zero));
-                son.transform.SetParent(this.transform);
+                son.SetActive(true);
 
             }
         }
