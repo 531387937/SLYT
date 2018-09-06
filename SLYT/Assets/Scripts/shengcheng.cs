a1dffacd8e18d07a2cf7b27412f0fca263c34f27
Zoryx <15801610913@163.com>
2018-09-06 10:51:47 +0800

    no message

---
+++ SLYT/Assets/Scripts/shengcheng.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shengcheng : MonoBehaviour {
+    public GameObject sc_;
+    public GameObject son;
+    float count = 0;
+    public float time_;
+    // Use this for initialization
+    void Start () {
+        son = GetComponentInChildren<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (son == null)
+        {
+            count += Time.deltaTime;
+            if (count > time_)
+            {
+                count = 0;
+                son = Instantiate(sc_, this.transform.position, Quaternion.Euler(Vector3.zero));
+                son.transform.SetParent(this.transform);
+
+            }
+        }
+    }
+}
