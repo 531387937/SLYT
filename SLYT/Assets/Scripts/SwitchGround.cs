2691aebd5b655c9a35bae401ebffd258c10890a3
Qizi <32046016+531387937@users.noreply.github.com>
2018-09-07 13:04:03 +0800

    damn 三角函数

---
+++ SLYT/Assets/Scripts/SwitchGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchGround : MonoBehaviour {
+    public GameObject ground;
+    private BoxCollider col;
+	// Use this for initialization
+	void Start () {
+        col = ground.GetComponent<BoxCollider>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "switch")
+        {
+            col.isTrigger = false;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "switch")
+        {
+            col.isTrigger = true;
+        }
+    }
+}
