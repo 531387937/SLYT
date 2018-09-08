6a4b1714be84e3eb89eb2a3693e3b17b53fb4f13
Qizi <531387937@qq.com>
2018-09-08 15:03:38 +0800

    Boss路径

---
+++ SLYT/Assets/Scripts/BossMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMove : MonoBehaviour {
+    public Transform[] trs;
+    private bool move=false;
+    private bool Touch=true;
+    public int pos = 0;
+    public static int i = 0;
+    // Use this for initialization
+    void Awake()
+    {
+        if (i == 0)
+        {
+            PlayerPrefs.SetFloat("boss_x", transform.position.x);
+            PlayerPrefs.SetFloat("boss_y", transform.position.y);
+            i++;
+        }
+        else;
+    }
+    // Use this for initialization
+    void Start () {
+        transform.position = new Vector3(PlayerPrefs.GetFloat("boss_x"), PlayerPrefs.GetFloat("boss_y"), 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(move)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, trs[pos - 1].position, 5f*Time.deltaTime);
+            if (gameObject.transform.position==trs[pos-1].position)
+            {
+                move = false;
+                Touch = true;
+                this.GetComponent<boss_shoot>().enabled = true;
+            }
+        }
+       
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player"&&Touch)
+        {
+            pos += 1;
+            move = true;
+            this.GetComponent<boss_shoot>().enabled = false;
+            Touch = false;
+        }
+    }
+}
--- SLYT/Assets/Scripts/savePlace.cs
+++ SLYT/Assets/Scripts/savePlace.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class savePlace : MonoBehaviour
 {
-
+    public GameObject BOSS;
+    private void Start()
+    {
+        BOSS = GameObject.FindGameObjectWithTag("boss");
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             PlayerPrefs.SetFloat("save_x", transform.position.x);
             PlayerPrefs.SetFloat("save_y", transform.position.y);
-
+            if(SceneManager.GetActiveScene().name=="boss")
+            {
+                PlayerPrefs.SetFloat("boss_x", BOSS.transform.position.x);
+                PlayerPrefs.SetFloat("boss_y", BOSS.transform.position.y);
+            }
         }
     }
 }
