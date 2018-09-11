f9a932d9a34720b368de37058b1f85d2c499332d
Zoryx <15801610913@163.com>
2018-09-11 15:45:12 +0800

    no message

--- SLYT/Assets/Scripts/Sign_Back.cs
+++ SLYT/Assets/Scripts/Sign_Back.cs
@@ -6,7 +6,7 @@ public class Sign_Back : MonoBehaviour {
     public GameObject player;
     private void Awake()
     {
-     
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     // Use this for initialization
     void Start () {
@@ -20,6 +20,13 @@ public class Sign_Back : MonoBehaviour {
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag=="sign")
+        {
+            player.gameObject.SendMessage("back");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "sign")
         {
             player.gameObject.SendMessage("back");
         }
