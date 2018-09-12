7513ff39369528c0dfd1a753d527d25dddc5525d
Qizi <531387937@qq.com>
2018-09-12 17:45:02 +0800

    震惊！

---
+++ SLYT/Assets/danger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class danger : MonoBehaviour {
+    public GameObject dangerous;
+    public GameObject Player;
+    private bool IsVisible;
+    private float an;
+    private float i = 0;
+	// Use this for initialization
+	void Start () {
+        Player=GameObject.FindGameObjectWithTag("Player");
+        dangerous = GameObject.Find("cone");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = transform.position.x-Player.transform.position.x;
+        float y =  transform.position.y-Player.transform.position.y;
+        an = Mathf.Atan(y / x);
+        if (y < 0 && x < 0)
+        {
+            an += Mathf.PI;
+        }
+        if (y > 0 && x < 0)
+        {
+            an += Mathf.PI;
+        }
+        if (!IsVisible)
+        {
+            dangerous.transform.GetChild(0).gameObject.SetActive(true);
+        }
+        if(IsVisible)
+        {
+            dangerous.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        dangerous.transform.position = new Vector3(Player.transform.position.x + 13 * Mathf.Cos(an), Player.transform.position.y + 13 * Mathf.Sin(an),0);
+	}
+    private void OnBecameInvisible()
+    {
+        IsVisible = false;
+        if(i==1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnBecameVisible()
+    {
+        IsVisible = true;
+        i = 1;
+    }
+}
