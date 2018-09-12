d2254f2e8348d6dfa4f5ea5c14c45301091ab311
Qizi <531387937@qq.com>
2018-09-12 16:25:09 +0800

    游戏暂停及选关

---
+++ SLYT/Assets/Scripts/StartManage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartManage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  //camare2D.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                SceneManager.LoadScene(hit.collider.gameObject.GetComponent<LoadScenes>().Which_Scene);
+            }
+        }
+
+    }
+}
